cec56e297d8794f6c8302afee461f30f63196415
Alex Barber <alexanderbdev@gmail.com>
2019-06-12 14:06:01 -0500

    NPM react-router-bootstrap, navbar skele, new favico

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,32 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Route, RouteHandler, Link} from 'react-router'
 import {logout} from '../store'
+import {Button, Nav, NavDropdown, MenuItem, NavItem} from 'react-bootstrap'
+import BSNavbar from 'react-bootstrap/Navbar'
+import {withRouter, Switch} from 'react-router-dom'
+import {LinkContainer} from 'react-router-bootstrap'
+import Routes from '../routes'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>MugShop ☕</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+  <BSNavbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+    <BSNavbar.Brand>MugShop</BSNavbar.Brand>
+    <LinkContainer to="/mugs">
+      <NavItem>Mugs</NavItem>
+    </LinkContainer>
+  </BSNavbar>
 )
+
+//  <LinkContainer to="/api/mugs">
+//     <NavItem>Mugs</NavItem>
+//   </LinkContainer>
+
+// const Navbar = ({handleClick, isLoggedIn}) => (
+//   <div>
+//     <h1>MugShop ☕</h1>
+//     <nav>
+//       {isLoggedIn ? (
+//         <div>
+//           {/* The navbar will show these links after you log in */}
+//           <Link to="/home">Home</Link>
+//           <a href="#" onClick={handleClick}>
+//             Logout
+//           </a>
+//         </div>
+//       ) : (
+//         <div>
+//           {/* The navbar will show these links before you log in */}
+//           <Link to="/login">Login</Link>
+//           <Link to="/signup">Sign Up</Link>
+//         </div>
+//       )}
+//     </nav>
+//     <hr />
+//   </div>
+// )
 
 /**
  * CONTAINER
