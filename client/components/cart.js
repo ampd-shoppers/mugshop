9796f7d32d42d0be79300aa4dcae392fc7d4c15d
Michelle Kiss <picturethis56@gmail.com>
2019-06-12 13:59:51 -0500

    cart component rendering with image and basic info

---
+++ client/components/cart.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Image from 'react-bootstrap/Image'
+
+const cartThings = [
+  {
+    id: 1,
+    name: 'Penguin',
+    currentPrice: '4.99',
+    quantity: 2,
+    imgSRC:
+      'https://images.unsplash.com/photo-1514228742587-6b1558fcca3d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjc0ODYxfQ'
+  },
+  {
+    id: 2,
+    name: 'Cat',
+    currentPrice: '2.99',
+    quantity: 1,
+    imgSRC:
+      'https://images.unsplash.com/photo-1539547256500-3c6cfd15bfdb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjc0ODYxfQ'
+  },
+  {
+    id: 3,
+    name: 'Dog',
+    currentPrice: '2.99',
+    quantity: 1,
+    imgSRC:
+      'https://images.unsplash.com/photo-1484981138541-3d074aa97716?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjc0ODYxfQ'
+  }
+]
+
+export class Cart extends Component {
+  //TODO: Add in the right props (map from state??)
+  render() {
+    return (
+      <div>
+        <h1>Shopping Cart</h1>
+        <ListGroup variant="flush">
+          {cartThings.map(item => (
+            <ListGroup.Item>
+              <Image src={item.imgSRC} /> {item.name} {item.quantity}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    )
+  }
+}
+
+export default Cart
--- client/routes.js
+++ client/routes.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
 import AllMugs from './components/all-mugs'
+import Cart from './components/cart'
 
 /**
  * COMPONENT
@@ -20,13 +21,14 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/mugs" component={AllMugs} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/mugs" component={AllMugs} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route exact path="/home" component={UserHome} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
