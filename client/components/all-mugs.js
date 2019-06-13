e3ea81792dfef45c445a7dbfdf545643f64f68be
Patricia de Boer <pdeboer05@gmail.com>
2019-06-13 14:24:45 -0500

    Mugs reducer created

--- client/components/all-mugs.js
+++ client/components/all-mugs.js
@@ -4,32 +4,41 @@ import CardGroup from 'react-bootstrap/CardGroup'
 import Axios from 'axios'
 import classNames from '../../public/style.css'
 
+import {connect} from 'react-redux'
+import {getAllMugs} from '../store'
+
 export class AllMugs extends Component {
-  constructor() {
-    super()
-    this.state = {
-      mugs: []
-    }
-  }
   //TODO: Add in the right props (map from state??)
   async componentDidMount() {
-    const response = await Axios.get('/api/mugs')
-    console.log(response)
-    // const mugs = response.data;
-    this.setState({mugs: response.data})
-    //console.log(this.state.mugs[0])
+    await this.props.fetchAllMugs()
+    // const response = await Axios.get('/api/mugs')
+    // console.log(response)
+    // // const mugs = response.data;
+    // this.setState({mugs: response.data})
+    // //console.log(this.state.mugs[0])
   }
   render() {
-    console.log(classNames.label)
+    console.log(this.props)
     return (
       <div>
         <CardGroup className={classNames.cardGroupMugs}>
-          {/* TODO: relink to correct mugs list not dummy data */}
-          {this.state.mugs.map(mug => <AllMugsCard key={mug.id} mug={mug} />)}
+          {/* {console.log(this.props)} */}
+          {this.props.mugs &&
+            this.props.mugs.map(mug => <AllMugsCard key={mug.id} mug={mug} />)}
         </CardGroup>
       </div>
     )
   }
 }
 
-export default AllMugs
+const mapState = state => {
+  console.log('state in mapState:', state)
+  return {mugs: state.mugs}
+}
+
+const mapDispatch = dispatch => {
+  return {
+    fetchAllMugs: () => dispatch(getAllMugs())
+  }
+}
+export default connect(mapState, mapDispatch)(AllMugs)
