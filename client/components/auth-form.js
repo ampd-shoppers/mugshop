4b96dc7187e6a27462a13e3ae5d52fb5d6bc6cbe
Patricia de Boer <pdeboer05@gmail.com>
2019-06-17 13:57:37 -0500

    fetches cart on log in

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import {auth, getCart} from '../store'
 
 /**
  * COMPONENT
@@ -59,12 +59,13 @@ const mapSignup = state => {
 
 const mapDispatch = dispatch => {
   return {
-    handleSubmit(evt) {
+    async handleSubmit(evt) {
       evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
-      dispatch(auth(email, password, formName))
+      await dispatch(auth(email, password, formName))
+      await dispatch(getCart())
     }
   }
 }
