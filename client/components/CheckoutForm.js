450291da4783bf11ab6e72473a397b3f9b804074
Michelle Kiss <picturethis56@gmail.com>
2019-06-19 09:35:32 -0500

    thank you page working

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react'
 import {CardElement, injectStripe} from 'react-stripe-elements'
+import ThankYou from './thank-you'
+import {BrowserRouter as Router, Redirect, withRouter} from 'react-router-dom'
 
 class CheckoutForm extends Component {
   constructor(props) {
     super(props)
-    this.state = {complete: false}
+    this.state = {
+      complete: false,
+      redirect: false
+    }
     this.submit = this.submit.bind(this)
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  // Renders the portion of the component that will cause the redirect...
+  renderRedirectToThankYou = () => {
+    if (this.state.redirect) {
+      return <Redirect to="user/checkout/success" />
+    }
   }
 
   async submit(ev) {
@@ -15,18 +33,28 @@ class CheckoutForm extends Component {
       headers: {'Content-Type': 'text/plain'},
       body: token.id
     })
-    this.props.checkoutProp()
-    if (response.ok) console.log('Purchase Complete!')
+    await this.props.checkoutProp()
+    if (response.ok) {
+      console.log('Purchase Complete!')
+      this.setRedirect()
+    }
   }
 
   render() {
-    if (this.state.complete) return <h1>Purchase Complete</h1>
+    if (this.state.complete)
+      return (
+        <div>
+          <h1>Purchase Complete</h1>
+          <ThankYou />
+        </div>
+      )
 
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
         <button onClick={this.submit}>Send</button>
+        {this.renderRedirectToThankYou()}
       </div>
     )
   }
